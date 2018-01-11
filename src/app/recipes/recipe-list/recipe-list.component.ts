e7b720f668696096ec5d88535720d13e35147ccc
nehalad <nlad1@binghamton.edu>
2018-01-11 12:50:06 -0500

    First

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model'
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
