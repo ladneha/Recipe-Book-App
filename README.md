86366d93831b5663b455da3f2beeeb08aa443bec
nehalad <nlad1@binghamton.edu>
2018-02-01 00:07:40 -0500

    new update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # RecipeBookApp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.3.
+This project displays receipes for various cuisines.
 
 ## Development server
 
--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
 import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
+
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
 
-  @Input() recipe : Recipe
-  @Output() recipeSelected = new EventEmitter<void>();
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
-  onSelected(){
-     this.recipeSelected.emit();
-  }
   ngOnInit() {
   }
-  
+
+  onSelected() {
+    this.recipeService.recipeSelected.emit(this.recipe);
+  }
+
 }
